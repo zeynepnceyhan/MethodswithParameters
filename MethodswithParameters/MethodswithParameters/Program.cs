fb5e4344e3270bf4a185aff2d25e114b36e00a97
Zeynep Ceyhan <104215922+zeynepnceyhan@users.noreply.github.com>
2022-08-01 14:01:01 +0300

    Methods with parameters
    internship

---
+++ MethodswithParameters/MethodswithParameters/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Add();
+            Add2(20, 30);
+            Console.ReadLine();
+        }
+        static void Add()
+        {
+            Console.WriteLine("Added!!");
+        }
+        static int Add2(int number1, int number2)
+        {
+            var result = number1 + number2;
+            Console.WriteLine("This numbers has been added!");
+            return result;
+        }
+
+
+    }
+}
